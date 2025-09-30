fc4c3702f6ddc0fa42054bf7b436e7f7b81976b5
Eliana Corujo <elianamariacorujo@gmail.com>
2025-09-30 19:58:50 -0300

    Páginas

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+# Importamos Flask y ahora también 'render_template'
+from flask import Flask, render_template
 
 # Creamos la aplicación
 app = Flask(__name__)
 
 # Definimos la ruta principal
 @app.route('/')
-def hola_mundo():
-    return '¡Mi app Opti-CV está funcionando!'
+def inicio():
+    # Ya no devolvemos un texto, sino que "renderizamos" nuestro archivo HTML
+    return render_template('index.html')
 
 # Esto es para poder ejecutar el servidor
 if __name__ == '__main__':
     app.run(debug=True)
+    
