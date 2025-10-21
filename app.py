1dc01a014065d3833fb9c51d0845d0446227d1a3
Eliana Corujo <elianamariacorujo@gmail.com>
2025-10-21 11:04:25 -0300

    Vinculando Postman 2

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from generador_pdf import crear_cv_pdf
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mi-clave-secreta-nadie-la-sabe'
 
 DB_USER = os.environ.get('DB_USER', 'postgres')
-DB_PASS = os.environ.get('DB_PASS', 'probar123') # O tu contraseña
+DB_PASS = os.environ.get('DB_PASS', 'probar123')
 DB_HOST = os.environ.get('DB_HOST', 'localhost')
 DB_NAME = os.environ.get('DB_NAME', 'opticv_db')
 
@@ -52,91 +53,4 @@ class Experiencia(db.Model):
     empresa = db.Column(db.String(100))
     fecha_inicio_exp = db.Column(db.String(50))
     fecha_fin_exp = db.Column(db.String(50))
-    descripcion_exp = db.Column(db.Text)
-    
-    cv_id = db.Column(db.Integer, db.ForeignKey('cv.id'), nullable=False)
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'cargo': self.cargo,
-            'empresa': self.empresa,
-            'fecha_inicio_exp': self.fecha_inicio_exp,
-            'fecha_fin_exp': self.fecha_fin_exp,
-            'descripcion_exp': self.descripcion_exp
-        }
-
-class Formacion(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100))
-    institucion = db.Column(db.String(100))
-    fecha_fin_formacion = db.Column(db.String(50))
-    
-    cv_id = db.Column(db.Integer, db.ForeignKey('cv.id'), nullable=False)
-    
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'titulo': self.titulo,
-            'institucion': self.institucion,
-            'fecha_fin_formacion': self.fecha_fin_formacion
-        }
-
-@app.route('/api/v1/cv', methods=['POST'])
-def create_cv():
-    data = request.json
-    
-    if not data or 'nombre_completo' not in data or 'email' not in data:
-        return jsonify({"error": "Faltan datos (nombre_completo, email)"}), 400
-
-    nuevo_cv = CV(
-        nombre_completo=data.get('nombre_completo'),
-        email=data.get('email'),
-        telefono=data.get('telefono'),
-        ubicacion=data.get('ubicacion'),
-        linkedin=data.get('linkedin'),
-        github=data.get('github'),
-        resumen=data.get('resumen'),
-        habilidades=data.get('habilidades'),
-        idiomas_certs=data.get('idiomas_certs')
-    )
-    db.session.add(nuevo_cv)
-    db.session.flush()
-
-    if 'experiencias' in data:
-        for exp_data in data['experiencias']:
-            exp = Experiencia(
-                cargo=exp_data.get('cargo'),
-                empresa=exp_data.get('empresa'),
-                fecha_inicio_exp=exp_data.get('fecha_inicio_exp'),
-                fecha_fin_exp=exp_data.get('fecha_fin_exp'),
-                descripcion_exp=exp_data.get('descripcion_exp'),
-                cv_id=nuevo_cv.id 
-            )
-            db.session.add(exp)
-
-    if 'formaciones' in data:
-         for form_data in data['formaciones']:
-            form = Formacion(
-                titulo=form_data.get('titulo'),
-                institucion=form_data.get('institucion'),
-                fecha_fin_formacion=form_data.get('fecha_fin_formacion'),
-                cv_id=nuevo_cv.id
-            )
-            db.session.add(form)
-            
-    db.session.commit()
-    
-    return jsonify(nuevo_cv.to_dict()), 201
-
-
-@app.route('/api/v1/cv/<int:cv_id>', methods=['GET'])
-def get_cv(cv_id):
-    cv = CV.query.get_or_404(cv_id)
-    return jsonify(cv.to_dict())
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        
-    app.run(debug=True)
+    descripcion_exp = db.Column
